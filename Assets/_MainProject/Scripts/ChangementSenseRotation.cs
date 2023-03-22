58be4d48560a7381831baffbb3aecad13b087ad7
thony311 <antoinelefebvrepoupa@hotmail.com>
2023-03-21 20:39:50 -0400

    Probleme Renard
    Les renards volent ou disparait

--- Assets/_MainProject/Scripts/ChangementSenseRotation.cs
+++ Assets/_MainProject/Scripts/ChangementSenseRotation.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ChangementSenseRotation : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> list = new List<GameObject>();
+    [SerializeField] private List<GameObject> _listGameObject = new List<GameObject>();
+    private List<ArbreTombant> _listeArbre = new List<ArbreTombant>();
     [SerializeField] private string _tag;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var arbre in _listGameObject)
+        {
+            _listeArbre.Add(arbre.GetComponent<ArbreTombant>());
+        }
     }
 
     // Update is called once per frame
@@ -22,8 +26,10 @@ public class ChangementSenseRotation : MonoBehaviour
     {
         if(other.gameObject.tag == _tag)
         {
-            Debug.Log("nice you got it");
-            
+            foreach(var arbre in _listeArbre)
+            {
+               arbre.ChangerSenseRotation();
+            }
         }
     }
 }
